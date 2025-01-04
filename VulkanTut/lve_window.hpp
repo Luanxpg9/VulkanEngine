b4f645bbfca0d5ce65627ebbcc0a04f4b7228d94
Luan Barbosa <luanxpg9@gmail.com>
2025-01-04 01:19:01 -0300

    implementing the swap shain

--- VulkanTut/lve_window.hpp
+++ VulkanTut/lve_window.hpp
@@ -12,7 +12,11 @@ class LveWindow {
 		LveWindow(int w, int h, std::string name);
 		~LveWindow();
 
+		LveWindow(const LveWindow&) = delete;
+		LveWindow &operator=(const LveWindow&) = delete;
+
 		bool shouldClose() { return glfwWindowShouldClose(window); }
+		VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; }
 
 		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
 
